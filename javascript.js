abe0781e02de252cf5ea5becba7b430373c9209e
Jake Egbert <jake.egbert04@gmail.com>
2025-08-22 10:24:28 -0600

    adds second challenge

--- javascript.js
+++ javascript.js
@@ -17,3 +17,17 @@ function highAndLow(numbers) {
 
 console.log(highAndLow("1 2 3 4 5")); // return "5 1"
 console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+// Examples:
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str) {
+  const result = [];
+
+  for (let i = 0; i < str.length; i += 2) {
+    result.push(str.substring(i, i + 2).padEnd(2, "_"));
+  }
+  return result;
+}
--- python.py
+++ python.py
@@ -17,3 +17,17 @@ def high_and_low(numbers):
 
 print(high_and_low("1 2 3 4 5"))  # returns "5 1"
 print(high_and_low("1 2 -3 4 5"))  # returns "5 -3"
+
+
+
+# Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+# Examples:
+# * 'abc' =>  ['ab', 'c_']
+# * 'abcdef' => ['ab', 'cd', 'ef']
+
+def solution(s):
+    result = []
+    for i in range(0, len(s), 2):
+        pair = s[i:i+2].ljust(2, "_")
+        result.append(pair)
+    return result
